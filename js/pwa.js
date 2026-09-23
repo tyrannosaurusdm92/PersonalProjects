51902e5932766b7ec3c3f95d83eb2e076ed9ab35
William Saville <tyrannosaurus.dm92@gmail.com>
2026-09-23 02:53:14 -0400

    Delete js directory

--- js/pwa.js
+++
@@ -1,9 +0,0 @@
-(function(global){
-'use strict';
-if(!('serviceWorker' in navigator))return;
-function register(){
-  if(location.protocol==='file:')return;
-  navigator.serviceWorker.register('./sw.js',{scope:'./'}).catch(()=>{});
-}
-if(document.readyState==='loading')document.addEventListener('DOMContentLoaded',register,{once:true});else register();
-})(window);
